7c687c84a96785b86c265fe748ce8bae35f82a23
Long Tran Phi <tplong@tma.com.vn>
2025-01-08 14:39:19 +0700

    parrallel route with @, intercepted route with (.) (..)(...)

---
+++ app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/card";
+
+export default function Login() {
+    return (
+        <Card>
+            <div>Please Login to continue</div>
+        </Card>
+
+    )
+
+}
